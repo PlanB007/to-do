197064763854d8a25c90d2ea5529db6f51214cbc
PlanB007 <kevinrouwkema@gmail.com>
2018-02-15 16:54:56 +0100

    Fixed local storage, still have to clean up the code tho..

--- script.js
+++ script.js
@@ -4,22 +4,48 @@ const submitField = document.querySelector('#submit_task');
 const allTasks = [];
 
 function checkForTasks() {
-  noTasksText.style.display = 'none';
-  if(allTasks.length == '0') {
-    noTasksText.style.display = 'inherit';
+  if(localStorage.getItem('tasks') === null) {
+	console.log('no local storage');
+	noTasksText.style.display = 'inherit';  
+  } else {
+	  console.log('storage detected');
+	  noTasksText.style.display = 'none';
+	  const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+	  console.log(savedTasks);
+
+	  for(let task of savedTasks) {
+
+			  console.log('func runned');
+			  const list = document.createElement('LI');
+			  list.textContent = task;
+			  taskList.appendChild(list);
+			  allTasks.push(task);
+
+	  }
   }
+
 }
 
 function getValue() {
-  const inputField = document.querySelector('#input_task').value;
+  const inputField = document.querySelector('#input_task');
   const listItem = document.createElement('LI');
-  const inputNode = document.createTextNode(inputField);
+  const inputNode = document.createTextNode(inputField.value);
 
+
+  allTasks.push(inputField.value);
+  localStorage.setItem('tasks', JSON.stringify(allTasks));
+  console.table(allTasks); 
+  
   listItem.appendChild(inputNode);
-  allTasks.push(inputField);
   taskList.appendChild(listItem);
+  
+  inputField.value = "";
 
-  checkForTasks();
+  noTasksText.style.display = 'none';
+
 }
 
+checkForTasks();
 submitField.addEventListener("click", getValue);
+
+//localStorage.clear();
